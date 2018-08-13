dcdf5f9679eb0b7ac4cd71ac16df3b6cd6c8cbdf
张晓钒 <zxv970209@outlook.com>
2018-08-13 16:03:23 +0800

    feature： 实现玩家在游戏边界内上下左右移动

--- js/app.js
+++ js/app.js
@@ -37,11 +37,38 @@ Player.prototype.update = function () {
 };
 //渲染玩家相关数据
 Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y-10);
 };
-
-Player.prototype.handleInput = function () {
-    
+//获取键盘操作并作出数据更新
+Player.prototype.handleInput = function (Keys) {
+    switch (Keys) {
+        case 'left':
+            this.x -= COL_WIDTH;
+            if (this.x < 0){
+                this.x = 0;
+            }
+            break;
+        case 'right':
+            this.x += COL_WIDTH;
+            if (this.x > COL_WIDTH * (numCol - 1)) {
+                this.x = COL_WIDTH * (numCol - 1);
+            }
+            break;
+        case 'up':
+            this.y -= ROW_WIDTH;
+            if (this.y < 0){
+                this.y = 0;
+            }
+            break;
+        case 'down':
+            this.y += ROW_WIDTH;
+            if (this.y > ROW_WIDTH * (numRow - 1)) {
+                this.y = ROW_WIDTH * (numRow - 1);
+            }
+            break;
+        default:
+            break;
+    }
 };
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
